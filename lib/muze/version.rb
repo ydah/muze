77d6556b0ab830acfa570f5c40e822baffaa20bc
ydah <t.yudai92@gmail.com>
2026-03-04 14:55:01 +0900

    Initial commit

---
+++ lib/muze/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Muze
+  VERSION = "0.1.0"
+end
---
+++ spec/muze_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Muze do
+  it "has a version number" do
+    expect(Muze::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
