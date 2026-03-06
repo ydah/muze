4d1fef65f6a1c6419bfb97029c3d23bbf8f24ff1
ydah <t.yudai92@gmail.com>
2026-03-06 23:33:07 +0900

    fix: avoid loading vorbis codec for wav backend

--- lib/muze/io/audio_loader/wavify_backend.rb
+++ lib/muze/io/audio_loader/wavify_backend.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
-require "wavify"
+require "wavify/errors"
+require "wavify/core/format"
+require "wavify/core/duration"
+require "wavify/core/sample_buffer"
+require "wavify/codecs/base"
+require "wavify/codecs/wav"
 
 module Muze
   module IO
     module AudioLoader
-      # WAV backend implemented with wavify.
+      # WAV backend implemented with wavify's pure-Ruby WAV codec surface.
       module WavifyBackend
         module_function
 
@@ -25,15 +30,15 @@ module Muze
         # @param path [String]
         # @return [Array(Array<Float>, Integer, Integer)]
         def read(path)
-          audio = Wavify::Audio.read(path)
-          float_format = audio.format.with(sample_format: :float, bit_depth: 32)
-          converted = audio.convert(float_format)
+          buffer = Wavify::Codecs::Wav.read(path)
+          float_format = buffer.format.with(sample_format: :float, bit_depth: 32)
+          converted = buffer.convert(float_format)
 
-          samples = samples_from_buffer(converted.buffer)
+          samples = samples_from_buffer(converted)
           [samples, converted.format.sample_rate, converted.format.channels]
         end
 
-        # @param buffer [Wavify::Buffer]
+        # @param buffer [Wavify::Core::SampleBuffer]
         # @return [Array<Float>, Array<Array<Float>>]
         def samples_from_buffer(buffer)
           return buffer.samples if buffer.format.channels == 1
